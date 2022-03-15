890f18f4bff97a115e94691ba6ca052e4789da6f
Gyungsoo <Gyungsoo@14.55.23.81>
2022-03-15 12:19:48 +0900

    [team3]init

---
+++ test/src/test/java/wizes/infoSystem/TestApplicationTests.java
@@ -0,0 +1,13 @@
+package wizes.infoSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
